9aa963a39e3278acbd702b34c1e62b4e0906eabf
Fran Čutura <francutura@gmail.com>
2023-04-01 11:56:26 +0200

    a :wq

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 from config import USER
 
-secret = None
+SECRET = None
 host = "http://192.168.1.101:3000"
 
 
@@ -23,11 +23,39 @@ def save_secret(secret):
 
 
 def load_secret():
+    global SECRET
     try:
         with open(".secret", "r") as file:
-            secret = file.read().strip()
+            SECRET = file.read().strip()
     except Exception:
         reg_user(USER)
+
+
+def get_all_pairs():
+
+    response = requests.get(f"{host}/getAllPairs")
+
+    if response.status_code == 200:
+        return json.dumps(response.json())
+    else:
+        print("FAILED: getAllPairs")
+
+
+def create_orders(orders: list):
+    """Create orders.
+
+    Orders = [(FROM, TO, VOLUME), (BTC, USD, 100)]
+
+    Args:
+        orders (list): orders
+    """
+    orders = "|".join(",".join(order) for order in orders)
+    response = requests.get(f"{host}/createOrders/{USER}/{SECRET}/{orders}")
+
+    if response.status_code == 200:
+        print(f"successfull trades: {orders}")
+    else:
+        print(f"FAILED: order ({orders})")
 
 
 def balance():
@@ -40,6 +68,9 @@ def balance():
 
 
 if __name__ == "__main__":
-    secret = load_secret()
-    bal = balance()
-    print(bal)
+    load_secret()
+    # print(get_all_pairs())
+    # exit()
+    print(balance())
+    create_orders([("USDT", "DUSK", "100")])
+    print(balance())
